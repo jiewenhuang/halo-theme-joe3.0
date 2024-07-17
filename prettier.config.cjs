f8bfa4759024034eeaed68656a123d04afd767b9
Ryan Wang <i@ryanc.cc>
2024-07-17 14:40:28 +0800

    chore: support auto-formatting code with Prettier
    Signed-off-by: Ryan Wang <i@ryanc.cc>

---
+++ prettier.config.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  tabWidth: 2,
+  useTabs: false,
+  endOfLine: "lf",
+  trailingComma: "es5",
+  singleQuote: true,
+  bracketSpacing: true,
+  arrowParens: "always",
+  printWidth: 100,
+};
