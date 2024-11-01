ac229c221015f5f6b112f040e12df1185a3dd213
AR-26710 <Eden1031@qq.com>
2024-11-01 23:25:09 +0800

    update：优化相册功能，简洁版，删除相册模板，仅添加分页功能，支持分页或加载；加载设置首页与相册单独设置

--- templates/assets/js/photos.js
+++ templates/assets/js/photos.js
@@ -6,49 +6,72 @@ $(document).ready(function(){
             columnWidth: '.grid-item'
         }
     });
-    let page = Number(document.querySelector('#image-grid').getAttribute('data-index'))+1
-    const totalPage = document.querySelector('#image-grid').getAttribute('data-total')
-
-    const baseUrl = ThemeConfig.blog_url;
+    let page = Number(document.querySelector('#image-grid').getAttribute('data-index')) + 1;
+    const totalPage = document.querySelector('#image-grid').getAttribute('data-total');
+    // 使用window.location.origin可避免跨域问题
+    const baseUrl = window.location.origin;
     const loadingIndicator = document.querySelector('.joe_loading');
 
-function loadRealImages(){
-    // 创建 Intersection Observer 实例
-    const gridItems = document.querySelectorAll('.grid-item');
+    function loadRealImages(){
+        // 创建 Intersection Observer 实例
+        const gridItems = document.querySelectorAll('.grid-item');
 
-    const observer = new IntersectionObserver(function(entries, observer) {
-        entries.forEach(function(entry) {
-            if (entry.isIntersecting) {
-                // 加载图片
-                const image = entry.target.querySelector('.lazy-load');
-                if (image) {
-                    image.src = image.dataset.src; // 将 data-src 的值赋给 src
-                    image.classList.remove('lazy-load'); // 移除 lazy-load 类
-                    observer.unobserve(entry.target); // 停止观察该元素
-                    image.onload = function() {
-                        $grid.isotope('layout');
-                    };
+        const observer = new IntersectionObserver(function(entries, observer) {
+            entries.forEach(function(entry) {
+                if (entry.isIntersecting) {
+                    // 加载图片
+                    const image = entry.target.querySelector('.lazy-load');
+                    if (image) {
+                        image.src = image.dataset.src; // 将 data-src 的值赋给 src
+                        image.classList.remove('lazy-load'); // 移除 lazy-load 类
+                        observer.unobserve(entry.target); // 停止观察该元素
+                        image.onload = function() {
+                            $grid.isotope('layout');
+                        };
+                    }
                 }
-            }
+            });
+        }, {
+            threshold: 0.5
         });
-    }, {
-        threshold: 0.5
-    });
 
-    // 开始观察每个 grid item
-    gridItems.forEach(function(item) {
-        observer.observe(item);
-    });
-}
-loadRealImages()
+        // 开始观察每个 grid item
+        gridItems.forEach(function(item) {
+            observer.observe(item);
+        });
+    }
+    loadRealImages();
 
+    /**
+     * 异步加载页面数据的函数
+     * 此函数根据当前URL和全局变量来确定要加载的页面数据的URL，并使用fetch API进行加载
+     * 加载成功后，它将解析HTML，提取并追加到页面上，同时更新全局页码变量
+     */
     const loadPageData = async function() {
-        const url = baseUrl + '/photos/page/' + page;
-        if(page>totalPage){
-            document.querySelector('.joe_loading').remove()
-            return
+        // 初始化URL变量
+        let url;
+        // 获取当前页面的URL
+        const currentUrl = window.location.href;
 
+        // 根据当前URL确定要加载的页面数据的URL
+        // 检查当前URL是否包含'/photos'且不包含'?group='
+        if (currentUrl.includes('/photos') && !currentUrl.includes('?group=')) {
+            url = `${baseUrl}/photos/page/${page}`;
+        // 检查当前URL是否包含'/photos'且包含'?group='
+        } else if (currentUrl.includes('/photos') && currentUrl.includes('?group=')) {
+            // 获取URL中的group参数
+            const groupParam = new URLSearchParams(window.location.search).get('group');
+            url = `${baseUrl}/photos/page/${page}?group=${groupParam}`;
         }
+
+        // 检查当前页码是否超过总页码
+        if (page > totalPage) {
+            // 移除加载指示器并返回
+            loadingIndicator.remove();
+            return;
+        }
+
+        // 使用fetch API加载确定的URL
         fetch(url)
             .then(response => response.text())
             .then(html => {
@@ -56,23 +79,25 @@ loadRealImages()
                 const doc = parser.parseFromString(html, 'text/html');
                 const nextPageImages = doc.querySelectorAll('.grid-item');
 
+                // 如果有图片元素，则追加到页面上
                 if (nextPageImages.length > 0) {
                     nextPageImages.forEach(image => {
                         $grid.append(image).isotope('appended', image);
                     });
                     $grid.isotope('layout');
-                    loadRealImages()
-                    page++
-                    if(page>totalPage){
-                        document.querySelector('.joe_loading').remove()
-
+                    // 加载实际图片并增加页码
+                    loadRealImages();
+                    page++;
+                    // 再次检查当前页码是否超过总页码
+                    if (page > totalPage) {
+                        // 移除加载指示器
+                        loadingIndicator.remove();
                     }
                 }
             })
             .catch(error => {
                 console.error('Error fetching next page data:', error);
             });
-
     };
 
     const observerForLoading = new IntersectionObserver(entries => {
@@ -83,7 +108,10 @@ loadRealImages()
         threshold: 1
     });
 
-    observerForLoading.observe(loadingIndicator);
+    // 使用try/catch隐藏在使用分页时浏览器控制台中报找不到元素的错
+    try {
+        observerForLoading.observe(loadingIndicator);
+    }catch (e){}
 
     $('.joe_photos__filter li').on('click', function() {
         const filterValue = $(this).attr('data-sjslink');
