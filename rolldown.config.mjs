fd8c278149c9c60ea74df38290fe30b3be8ba1a4
Ryan Wang <i@ryanc.cc>
2026-01-06 22:05:02 +0800

    Migrate to rolldown

--- rolldown.config.mjs
+++ rolldown.config.mjs
@@ -1,11 +1,9 @@
-import resolve from "@rollup/plugin-node-resolve";
-import { readdirSync } from "fs";
-import { join as joinPath, resolve as resolvePath } from "path";
+import { readdirSync } from "node:fs";
+import { join as joinPath, resolve as resolvePath } from "node:path";
 import postcss from "rollup-plugin-postcss";
-import { terser } from "rollup-plugin-terser";
 
-const jsRootDir = resolvePath(__dirname, "templates", "assets", "js");
-const lessRootDir = resolvePath(__dirname, "templates", "assets", "css");
+const jsRootDir = resolvePath(import.meta.dirname, "templates", "assets", "js");
+const lessRootDir = resolvePath(import.meta.dirname, "templates", "assets", "css");
 const jsFiles = readdirSync(jsRootDir).filter((file) => file.endsWith(".js"));
 const lessFiles = readdirSync(lessRootDir).filter((file) =>
   file.endsWith(".less")
@@ -16,8 +14,8 @@ const jsConfig = jsFiles.map((file) => ({
   output: {
     file: joinPath(jsRootDir, "min", file.replace(".js", ".min.js")),
     format: "iife",
+    minify: true,
   },
-  plugins: [resolve(), terser()],
   treeshake: false,
 }));
 
@@ -25,6 +23,7 @@ const lessConfig = lessFiles.map((file) => ({
   input: joinPath(lessRootDir, file),
   output: {
     file: joinPath(lessRootDir, "min", file.replace(".less", ".min.css")),
+    minify: true,
   },
   plugins: [
     postcss({
